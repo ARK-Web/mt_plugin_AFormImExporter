916fdfd5e4f31584271167afba09e36b3f150e5c
tiadeen2 <tiadeen2@yahoo.co.jp>
2024-04-11 12:33:52 +0900

    refs #62687 インポート元とエクスポート先のAFormIDがズレていても動くようにした

--- plugins/AFormImExporter/lib/AFormImExporter/CMS.pm
+++ plugins/AFormImExporter/lib/AFormImExporter/CMS.pm
@@ -8,9 +8,29 @@ use MT::AForm;
 
 sub import_aform {
     my $app = shift;
+
+	my @sites;
+	my $site_iter = MT::Website->load_iter();
+	while (my $site = $site_iter->()) {
+		push @sites, {
+			id => $site->id,
+			name => $site->name,
+		};
+		my @blogs = @{$site->blogs};
+		foreach my $blog (@blogs) {
+			push @sites, {
+				id => $blog->id,
+				name => "-- ". $blog->name,
+			};
+		}
+	}
+	my @aforms = MT::AForm->load();
+
 	my %param = (
 		'import_success' => $app->param('import_success') || '',
 		'aform_id' => $app->param('aform_id') || '',
+		'sites' => \@sites,
+		'aforms' => \@aforms,
 	);
 	my $html = $app->load_tmpl('import_aform.tmpl', \%param);
 	return $app->build_page($html, \%param);
@@ -25,6 +45,41 @@ sub import_aform_process {
 	my $filename = $q->tmpFileName($fh);
 	my( $version, %aform_values, @fields );
 	_read_aform_dat($filename, \$version, \%aform_values, \@fields);
+
+	# import blog
+	my $import_blog = $app->param('import_blog') || '';
+	if ($import_blog eq 'global') {
+		$aform_values{'blog_id'} = 0;
+	}
+	elsif ($import_blog eq 'same') {
+		# nothing to do
+	}
+	elsif ($import_blog eq 'other') {
+		my $other_blog = $app->param('other_blog') || undef;
+		return $app->error($app->translate("Please select blog")) unless $other_blog;
+		$aform_values{'blog_id'} = $other_blog;
+	}
+	else {
+		return $app->error($app->translate("Please select import blog"));
+	}
+
+	# import form
+	my $import_form = $app->param('import_form') || '';
+	if ($import_form eq 'new') {
+		$aform_values{'id'} = undef;
+	}
+	elsif ($import_form eq 'same') {
+		# nothing to do
+	}
+	elsif ($import_form eq 'other') {
+		my $other_form = $app->param('other_form') || undef;
+		return $app->error($app->translate("Please select form")) unless $other_form;
+		$aform_values{'id'} = $other_form;
+	}
+	else {
+		return $app->error($app->translate("Please select import form"));
+	}
+
 	my $aform_id = $aform_values{'id'};
 
 	# check version
@@ -41,17 +96,20 @@ sub import_aform_process {
 	}
 	$aform->save()
 		or return $app->error( $app->translate("save aform error") . ':' . $aform->errstr );
+	$aform_id = $aform->id;
 
 	# delete all aform_fields
-	MT::AFormField->remove({ 'aform_id' => $aform_id });
+	MT::AFormField->remove({ 'aform_id' => $aform_id }) if $aform_id;
 
 	# save aform_fields
-	my $aform_field = MT::AFormField->new;
 	foreach my $tmp (@fields) {
+		my $aform_field = MT::AFormField->new;
 		my( %field_values ) = @$tmp;
 		foreach my $key (keys %field_values) {
 			$aform_field->set_values({ $key => $field_values{$key} });
 		}
+		$aform_field->id(undef);
+		$aform_field->aform_id($aform_id);
 		$aform_field->save()
 			or return $app->error( $app->translate("save aform_field error") . ':' . $aform_field->errstr );
 	}
@@ -73,6 +131,7 @@ sub export_aform {
 sub export_aform_process {
 	my $app = shift;
 
+eval {
 	my $aform_id = $app->param('aform_id')
 		or return $app->error( $app->translate("Invalid request") );
 
@@ -91,10 +150,11 @@ sub export_aform_process {
 	$app->set_header( "Cache-Control" => "public" );
 	$app->set_header( "Pragma" => "public" );
 	$app->set_header( "Content-Disposition" => "attachment; filename=$file" );
-	$app->send_http_header( "text/plain" );
+	$app->send_http_header( "text/plain; charset=utf-8" );
 
+	my $output;
 	# output version
-	print $aform_version . "\n";
+	$output = $aform_version . "\n";
 
 	# output aform
 	my @aform_values = ();
@@ -103,7 +163,7 @@ sub export_aform_process {
 		push @aform_values, $col;
 		push @aform_values,	$column_values{$col};
 	}
-	print _make_csv(@aform_values) . "\n";
+	$output .= _make_csv(@aform_values) . "\n";
 
 	# output aform_fields
 	foreach my $aform_field (@aform_fields){
@@ -113,8 +173,15 @@ sub export_aform_process {
 			push @aform_field_values, $col;
 			push @aform_field_values, $column_values{$col};
 		}
-		print _make_csv(@aform_field_values) . "\n";
+		$output .= _make_csv(@aform_field_values) . "\n";
 	}
+	$app->print(Encode::encode("utf8", $output));
+};
+if ($@) {
+	my $err = $@;
+	$app->log($err);
+	return $app->error($err);
+}
 
 	return 1;
 }
--- plugins/AFormImExporter/lib/AFormImExporter/L10N/ja.pm
+++ plugins/AFormImExporter/lib/AFormImExporter/L10N/ja.pm
@@ -19,11 +19,21 @@ use vars qw( %Lexicon );
 	'Export A-Form' => 'A-Formエクスポート',
 	'Import description' => '<h4>注意点</h4>
 <ul>
-<li>インポートするデータと同じIDのフォームがある場合、上書きされます。</li>
 <li>バージョンの違うデータはインポートできません。</li>
 <li>インポートするだけです。再構築などは手動で行ってください。</li>
 </ul>',
 	'Import successed' => '正常にインポートされました',
 	'A-Form version error' => 'A-Formのバージョンが違います',
+	'Import file' => 'インポートするファイル',
+	'Import form' => 'インポート先のフォーム',
+	'Import form hint' => 'インポート先のフォームを指定してください。',
+	'New form' => '新規フォーム(新しくフォームを作成します)',
+	'Same form' => '同じフォーム(インポートファイルと同じフォームIDのフォームを更新します)',
+	'Other form' => '他のフォーム',
+	'Please select form' => 'フォームを選択してください',
+	'Import blog' => 'インポート先のサイト',
+	'Same blog' => '同じサイト(インポートファイルと同じサイトIDのフォームとします)',
+	'Other blog' => '他のサイト',
+	'Please select blog' => 'サイトを選択してください',
 );
 1;
